814259d93f4e572aedf2ac787724e284c665ee40
saurav <saurav@sauravpc>
2017-04-02 09:38:03 -0700

    Signed-off-by: saurav <saurav@sauravpc>

--- src/test/java/pages/Loginpage.java
+++ src/test/java/pages/Loginpage.java
@@ -1,11 +1,9 @@
 package pages;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.DataProvider;
 
 import factory.DataProviderFactory;
